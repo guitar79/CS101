c62d5da935dc8930eef8137b557bf8638053448b
guitar79 <guitar79@naver.com>
2021-04-01 14:02:50 +0900

    update

---
+++ assignment_01-1.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from cs1robots import *
+
+load_world('worlds/hurdles1.wld')
+
+gshs = Robot()
+gshs.set_trace('blue')
+
+def turn_right():
+    gshs.turn_left()
+    gshs.turn_left()
+    gshs.turn_left()
+
+def turn_around():
+    gshs.turn_left()
+    gshs.turn_left()
+
+def climb_up_1_stair():
+    gshs.turn_left()
+    gshs.move()
+    gshs.turn_right()
+    gshs.move()
+    gshs.move()
+
+def climb_up_4_stairs():
+    climb_up_1_stair()
+    climb_up_1_stair()
+    climb_up_1_stair()
+    climb_up_1_stair()
+
+def exercise_1():
+    gshs.move()
+    gshs.turn_left()
+    gshs.move()
+    turn_right()
+    gshs.move()
+    turn_right()
+    gshs.move()
+    gshs.turn_left()
+
+for i in range(4):
+    exercise_1()
+gshs.move()
+gshs.pick_beeper()
--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def exercise_2(j, i):
     move_1(j)
     turn_around()
 
+
 for i in range(6):
     for j in range(1, 7):
         exercise_2(j, i)
